e6379ec409731c607675c803619cad03efadf60a
sabbir <sabbiramin.cse11ruet@gmail.com>
2021-07-17 21:04:46 +0600

    Primary Refactoring

--- menu_app/admin.py
+++ menu_app/admin.py
@@ -9,8 +9,13 @@ email: sabbiramin.cse11ruet@gmail.com, sabbir@rokomari.com
 from django.contrib import admin
 
 # Register your models here.
-
+from menu_app.models import Restaurant, Item, Menu, Vote
 
 admin.site.site_header = 'Taste-Monger'
 admin.site.site_title = 'Taste-Monger 1.0'
 admin.site.index_title = 'Welcome to Taste-Monger Admin 1.0'
+
+admin.site.register(Restaurant)
+admin.site.register(Item)
+admin.site.register(Menu)
+admin.site.register(Vote)
--- menu_app/models.py
+++ menu_app/models.py
@@ -32,17 +32,17 @@ food_type = [
 
 
 class Item(models.Model):
-    name = models.CharField(null=False, max_length=50)
-    ingredients = models.TextField(null=False)
-    item_type = models.CharField(null=False, choices=food_type, default='MAIN_DISH')
+    name = models.CharField(null=False, unique=True, max_length=50)
+    ingredients = models.TextField(null=False, default='')
+    item_type = models.CharField(null=False, max_length=50, choices=food_type, default='MAIN_DISH')
 
     def __str__(self):
         return self.name
 
 
 class Menu(models.Model):
-    restaurant = models.ForeignKey(Restaurant, related_name='restaurant', null=False, on_delete=models.SET_NULL)
-    item = models.ForeignKey(Item, related_name='item', null=False, on_delete=models.SET_NULL)
+    restaurant = models.ForeignKey(Restaurant, related_name='restaurant', null=False, on_delete=models.CASCADE)
+    item = models.ForeignKey(Item, related_name='item', null=False, on_delete=models.CASCADE)
     c_date = models.DateField(default=datetime.date)  # Todo:// Need to restrict editing any past menuItem
     vote_count = models.IntegerField(default=0)
 
@@ -52,10 +52,9 @@ class Menu(models.Model):
 
 class Vote(models.Model):
     # Todo:// allow user to only vote for current day
-    menu = models.ForeignKey(Menu, related_name='menu', null=False, on_delete=models.SET_NULL)
-    user = models.ForeignKey(UserModel, null=False, related_name='users', on_delete=models.SET_NULL)
+    menu = models.ForeignKey(Menu, related_name='menu', null=False, on_delete=models.CASCADE)
+    user = models.ForeignKey(UserModel, null=False, related_name='users', on_delete=models.CASCADE)
     hasLiked = models.BooleanField(default=False)
 
     def __str__(self):
         return '{} - liked - {}'.format(self.user.username, self.menu)
-
